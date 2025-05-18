2d1ea2e1799b55dd4b6e7a15f39df41e37747ac2
monqka0201 <sca_mqm1@yahoo.co.jp>
2025-05-19 08:08:37 +0900

    課題の提出

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\support\facades\DB;
 
 class PostController extends Controller
 {
     public function index() {
-        return view('posts.index');
+        // クエリビルダでpostsテーブルの全データを取得
+        $posts = DB::table('posts')->get();
+        return view('posts.index', compact('posts'));
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,1 +1,25 @@
-<h1>投稿一覧</h1>
+<!DOCTYPE html>
+<html lamg="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Laravel基礎</title>
+</head>
+
+<body>
+  <h1>投稿一覧</h1>
+  <table>
+    <tr>
+      <td>タイトル</td>
+      <td>本文</td>
+    </tr>
+    @foreach($posts as $post)
+    <tr>
+      <td>{{ $post->title }}</td>
+      <td>{{ $post->content }}</td>
+    </tr>
+    @endforeach
+  </table>
+</body>
+</html>
